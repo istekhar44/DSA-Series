306d16d5711deab7bea3d3e2943a07e04cc8c531
istekhar44 <67890istekharalam@gmail.com>
2025-02-14 23:45:24 -0800

    commit

--- Array/sum_array.cpp
+++ Array/sum_array.cpp
@@ -1,28 +1,7 @@
 #include <iostream>
 using namespace std;
-
-int arrSum(int arr[], int size) {
-    int sum = 0;
-    for (int i = 0; i < size; i++) {
-        sum += arr[i];
-    }
-    return sum;
-}
-
-int main() {
-    int size = 5;
-    int arr[size];
-
-    cout << "Enter the elements: " << endl;
-    for (int i = 0; i < size; i++) {
-        cin >> arr[i];
-    }
-
-    // Call the function
-    int sum = arrSum(arr, size);
-
-    // Output the sum
-    cout << "Sum of the array elements: " << sum << endl;
-
+int main(){
+    cout<<"helllow"<<endl;
     return 0;
+
 }
