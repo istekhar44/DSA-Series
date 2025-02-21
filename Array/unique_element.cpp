0833cd0603975bd577219f5095c938ed732a1c00
istekhar44 <67890istekharalam@gmail.com>
2025-02-20 22:12:35 -0800

    commit

---
+++ Array/unique_element.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr []= {-3, 0, 1, -3, 1, 1, 1, -3, 10, 0};
+    int size = sizeof(arr)/ sizeof(arr[i]);
+}
--- array2.cpp/leetcode.cpp
+++ array2.cpp/leetcode.cpp
@@ -14,3 +14,16 @@ int main() {
     cout << "Result: " << ans << endl; // Print the result
     return 0; // Return 0 to indicate successful execution
 }
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int arr[7]={5,6,7,6,5,7,4};
+//     int size =sizeof(arr)/ sizeof(arr[0]);
+//     for(int i =0; i<size ;i++){
+      
+//         int ans =ans ^ arr[i];
+//     }
+//     cout <<"result"<< " " << endl;
+//     return 0;
+
+}
