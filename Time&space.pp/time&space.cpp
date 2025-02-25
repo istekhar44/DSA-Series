9493478f4622f5c94f58b66dc31201b67518e689
istekhar44 <67890istekharalam@gmail.com>
2025-02-25 04:56:14 -0800

    commit

---
+++ Time&space.pp/time&space.cpp
@@ -0,0 +1,10 @@
+// Time Complexity?
+// 1. It is the amount of time taken by an algorithm to run.
+// 2. As a function of the length.
+#include <iostream>
+using namespace std;
+int main(){
+    
+    
+
+}
--- array2.cpp/swap_array.cpp
+++ array2.cpp/swap_array.cpp
@@ -1,31 +1,34 @@
 #include<iostream>
 using namespace std;
 
-//using n in the place of size
+// Function to swap alternate elements in the array
 void swapAlter(int arr[], int n){
-    for(int i=0; i<n ; i+=2){
-        if(i+1 < n){
-            swap(arr[i], arr[i+1]);
-
+    for(int i = 0; i < n; i += 2){
+        if(i + 1 < n){
+            swap(arr[i], arr[i + 1]);
         }
     }
-    
 }
 
-
-void printArray(arr[]; int n){
-
-    for(int i=0; i<=n: i++){
-        cout<<array[i]<<" ";
+// Function to print the array
+void printArray(int arr[], int n){
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
     }
-int main(){
-    int arr[8]= {9,10,8,7,6,5,4,3};
-    int arr[5]= {11,33,55,77,10};
-    swapAlter(even, 8);
-    printArray(even,8);
-    cout<<endl;
+    cout << endl;
+}
 
-   
+int main(){
+    int even[8] = {9, 10, 8, 7, 6, 5, 4, 3};
+    int odd[5] = {11, 33, 55, 77, 10};
+
+    // Swap alternate elements in the even array
+    swapAlter(even, 8);
+    printArray(even, 8);
+
+    // Swap alternate elements in the odd array
+    swapAlter(odd, 5);
+    printArray(odd, 5);
 
     return 0;
 }
