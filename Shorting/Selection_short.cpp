82a8753af08625ee929185cd7946b76ed3e6b18b
istekhar44 <67890istekharalam@gmail.com>
2025-03-29 23:44:46 -0700

    selection Short

--- Shorting/Selection_short.cpp
+++ Shorting/Selection_short.cpp
@@ -1,6 +1,32 @@
 #include<iostream>
 using namespace std;
-
+void selectionShort( int arr[] , int n){
+    for(int i= 0 ; i < n-1 ; i++){
+        int minIndex = i;
+        for(int j =i+1  ;j < n ; j++){
+            if (arr[j] < arr [minIndex])
+            minIndex  = j;
+            swap( arr[minIndex] , arr[i]);
+        }
+    } 
+}
+void printArray(int arr[] , int n){
+    for(int i = 0 ; i < n ; i++){
+        cout<< arr[i] << " ";
+    }
+    cout << endl;
+}
 int main(){
+    int arr[5] = {8, 4,9,7,10};
+    
+    int n = sizeof(arr)/ sizeof(arr[0]);
+
+    cout << " print the accutal array" ;
+    printArray(arr , n);
+
+    cout << "print the shorted Array";
+    selectionShort(arr , n);
+    printArray(arr ,n);
+
 return 0;
 }
