f621ff1e6ee3079ebcf03871ea1e445db6fee507
istekhar44 <67890istekharalam@gmail.com>
2025-04-20 22:13:11 -0700

    commit

--- Pointer/PointerArray.cpp
+++ Pointer/PointerArray.cpp
@@ -3,33 +3,51 @@
 using namespace std;
 
 int main(){
-int array[5] = {3,5,6,7,9};
-int *ptr = &array[5];
+// int array[5] = {3,5,6,7,9};
+// int *ptr = &array[5];
 
-cout<<"print the value of array" <<&array<<" "<<endl;
-cout <<"Adress of the array"<<ptr<<endl;
+// cout<<"print the value of array" <<&array<<" "<<endl;
+// cout <<"Adress of the array"<<ptr<<endl;
 
-//print the type of the array 
-cout <<"Type of the array"<<typeid(array).name()<<endl;
+// //print the type of the array 
+// cout <<"Type of the array"<<typeid(array).name()<<endl;
 
-//print the first element of the array
-cout <<"1th element"<<*array<<endl;
+// //print the first element of the array
+// cout <<"1th element"<<*array<<endl;
 
-//pluse 1 the first element of the array
-cout <<"1rd element of the array "<<*array+1<<endl;
+// //pluse 1 the first element of the array
+// cout <<"1rd element of the array "<<*array+1<<endl;
 
-//fouth element of the array
-cout <<"print the fourth element"<<array[4]<<endl;
+// //fouth element of the array
+// cout <<"print the fourth element"<<array[4]<<endl;
 
-//print the first elemnet of the array
-cout <<(*array)<<endl;
+// //print the first elemnet of the array
+// cout <<(*array)<<endl;
 
-cout 
+// int temp [10];
+// cout << sizeof(temp) << endl;
+
+// int *ptr = &temp[0];
+// cout << sizeof(ptr)<<endl;
+
+// cout <<sizeof(*ptr)<<endl;
+
+// cout <<sizeof(&ptr)<<endl;
+
+int a[20] = {3,5,6,7,6};
+ cout << &a<<endl;
+ cout << a<< endl;
+ cout <<&a[0] <<endl;
+
+ int *p = &a[20];
+ 
 
 
 
 
 
+
+return 0;
 
 
 }
