e4f0b0428747d06b580fb95aff7fb42f0fbf232e
istekhar44 <67890istekharalam@gmail.com>
2025-04-28 08:27:55 -0700

    add the new file

--- OOPs/oops_basic.cpp
+++ OOPs/oops_basic.cpp
@@ -1,11 +1,23 @@
 #include<iostream>
 using namespace std;
+//class 1
 class student {
     public:
     string name ;
     float marks;
     int rollNo;
     char hellow;
+
+};
+//class 2
+class car {
+    public ;
+    string brand;
+    int price ;
+    string type;
+
+    
+
 
 };
 
@@ -16,8 +28,13 @@ int main(){
     // s1.rollNo = 43;
     cout <<"Enter the roll NO";
     cin >>s1.rollNo;
-
     s1.hellow = 'c';
+
+    car c1;
+    c1.brand = "honda ";
+    c1.price = 10000;
+    c1.type = "luxery";
+    
 
     cout <<"Name "<<s1.name << "Marks" << s1.marks<<endl;
 
