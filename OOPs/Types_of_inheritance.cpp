cccd70e3b7feafd59587d23c1b3630ba888adb28
istekhar44 <67890istekharalam@gmail.com>
2025-04-30 07:34:17 -0700

    commit the heap file

---
+++ OOPs/Types_of_inheritance.cpp
@@ -0,0 +1,25 @@
+// // Types of inheritance : There are five type of the inheritance.
+// ✅ 1. Single Inheritance
+// One class inherits from one base class.
+// Example: A Dog class inherits from an Animal class.
+
+// ✅ 2. Multiple Inheritance
+// One class inherits from two or more base classes.
+// It combines features from multiple parent classes into one child class.
+
+// ✅ 3. Multilevel Inheritance
+// Inheritance occurs in multiple levels.
+// A class is derived from another class, which is already a derived class.
+
+// ✅ 4. Hierarchical Inheritance
+// Multiple classes inherit from the same base class.
+// Useful when different subclasses share some common features.
+
+// ✅ 5. Hybrid Inheritance
+// A combination of two or more types of inheritance.
+// Often used in complex programs and may lead to ambiguity if not handled carefully.
+#include<iostream>
+using namespace std;
+int main(){
+return 0;
+}
