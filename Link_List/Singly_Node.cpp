ebf0787cfc1d238e3128a07a69177cfe8448a97e
istekhar44 <67890istekharalam@gmail.com>
2025-05-05 06:45:43 -0700

    add the singly linked list

--- Link_List/Singly_Node.cpp
+++ Link_List/Singly_Node.cpp
@@ -5,3 +5,50 @@
 //   1.The first node is known as the head of the list.
 //   2.The last node points to NULL, indicating the end of the list.
 
+
+// print the simple node
+
+// #include <iostream>
+// using namespace std ;
+// class Node {
+//     public:
+//     int value ;
+//     Node* next ;
+// };
+//  int main (){
+//     Node* node1= new Node();
+//     cout<< "print node: "<<node1 << node1 -> value <<endl;
+//     cout<<node1 -> next<<endl;
+//     return 0;
+//  }
+
+
+
+// Make the singly linked list 
+#include <iostream>
+using namespace std;
+class Node {
+    public :
+    int value ;
+    Node* next ;
+
+
+    //make the second node 
+
+    string name ;
+    Node* next;
+
+    //make the constructor to add the new node 
+    Node (int data ){
+        this -> next = data ;
+        this -> null = Null;
+
+    }
+    
+
+};
+int main(){
+    //Call the next value 
+    
+
+}
