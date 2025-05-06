fddc5dee077fff7f737f93094dcab2b81bc98830
istekhar44 <67890istekharalam@gmail.com>
2025-05-05 21:51:14 -0700

    commit the code

--- Link_List/Singly_Node.cpp
+++ Link_List/Singly_Node.cpp
@@ -22,33 +22,142 @@
 //     return 0;
 //  }
 
-
+            // Algorithm With Code 
 
 // Make the singly linked list 
+// #include <iostream>
+// using namespace std;
+// class Node {
+//     public :
+
+//     //int type linked list  : make the node 
+//     int data;
+//     Node* next ;
+
+
+//     //string type linked list : make the node 
+//     // string name ;
+//     // Node* next;
+
+//     //make the constructor to add the new node 
+//     Node (int data ){
+//         this -> next = data ;
+//         this -> null = Null;
+
+//     }
+    
+
+// };
+
+// //insert the node at the head of the list 
+// void insertAtHead(Node* &head, int d){
+//     Node* temp = new Node(d);  //create the temp node and make the head of the list 
+//     temp -> next = head;       //point the temp to head
+//     head =temp;            // temp ko head bol dia 
+// }
+
+
+// //insert the element at the tail of the Node 
+// void  insertAtTail(Node* &tail, int d){   //tail ma data insert kera dia 
+//     Node* temp = new Node(d);           //temp name ki new node bana di
+//     tail ->next = temp;           //temp ko next ko tail bana dia
+//     temp = tail -> next;
+// }
+
+
+
+
+//  void printLinkedList(Node* &head){    //print the list
+//     node *temp = head;                
+//     while (temp != Null){            // loop laga dia jab tak temp Null ka barabar nahi hota tab tak print ker
+//         cout<<temp ->data << " ";
+//         temp = temp ->next;          //temp ko next pa point kera dia
+
+//     }
+//     cout <<endl;
+//  }
+
+// int main(){
+//    // Create the first node
+//    Node* node1 = new Node(20);
+//    Node* head = node1;
+//    Node* tail = node1;
+
+//    printLinkedList(head);
+
+//    // Insert at head
+//    insertAtHead(head, 10);
+//    printLinkedList(head);
+
+//    // Insert at tail
+//    insertAtTail(tail, 30);
+//    printLinkedList(head);
+
+//    return 0;
+
+
+// }
+
+
+
+//above Correct code 
+
 #include <iostream>
 using namespace std;
+
+// Define the Node class
 class Node {
-    public :
-    int value ;
-    Node* next ;
-
-
-    //make the second node 
-
-    string name ;
+public:
+    int data;
     Node* next;
 
-    //make the constructor to add the new node 
-    Node (int data ){
-        this -> next = data ;
-        this -> null = Null;
-
+    // Constructor to initialize the node
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
     }
-    
-
 };
-int main(){
-    //Call the next value 
-    
 
+// Insert a new node at the head of the list
+void insertAtHead(Node*& head, int d) {
+    Node* temp = new Node(d);
+    temp->next = head;
+    head = temp;
 }
+
+// Insert a new node at the tail of the list
+void insertAtTail(Node*& tail, int d) {
+    Node* temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+// Print the linked list
+void printLinkedList(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Create the first node
+    Node* node1 = new Node(20);
+    Node* head = node1;
+    Node* tail = node1;
+
+    printLinkedList(head);
+
+    // Insert at head
+    insertAtHead(head, 10);
+    printLinkedList(head);
+
+    // Insert at tail
+    insertAtTail(tail, 30);
+    printLinkedList(head);
+
+    return 0;
+}
+
--- Pointer/PointerArray.cpp
+++ Pointer/PointerArray.cpp
@@ -2,71 +2,71 @@
 #include <typeinfo>
 using namespace std;
 
-// void print(char *p){
-//     cout << *p<< endl;
-// }
+void print(char *p){
+    cout << *p<< endl;
+}
 
 int main(){
-// int array[5] = {3,5,6,7,9};
-// int *ptr = &array[5];
+int array[5] = {3,5,6,7,9};
+int *ptr = &array[5];
 
-// cout<<"print the value of array" <<&array<<" "<<endl;
-// cout <<"Adress of the array"<<ptr<<endl;
+cout<<"print the value of array" <<&array<<" "<<endl;
+cout <<"Adress of the array"<<ptr<<endl;
 
-// //print the type of the array 
-// cout <<"Type of the array"<<typeid(array).name()<<endl;
+//print the type of the array 
+cout <<"Type of the array"<<typeid(array).name()<<endl;
 
-// //print the first element of the array
-// cout <<"1th element"<<*array<<endl;
+//print the first element of the array
+cout <<"1th element"<<*array<<endl;
 
-// //pluse 1 the first element of the array
-// cout <<"1rd element of the array "<<*array+1<<endl;
+//pluse 1 the first element of the array
+cout <<"1rd element of the array "<<*array+1<<endl;
 
-// //fouth element of the array
-// cout <<"print the fourth element"<<array[4]<<endl;
+//fouth element of the array
+cout <<"print the fourth element"<<array[4]<<endl;
 
-// //print the first elemnet of the array
-// cout <<(*array)<<endl;
+//print the first elemnet of the array
+cout <<(*array)<<endl;
 
-// int temp [10];
-// cout << sizeof(temp) << endl;
+int temp [10];
+cout << sizeof(temp) << endl;
 
-// int *ptr = &temp[0];
-// cout << sizeof(ptr)<<endl;
+int *ptr = &temp[0];
+cout << sizeof(ptr)<<endl;
 
-// cout <<sizeof(*ptr)<<endl;
+cout <<sizeof(*ptr)<<endl;
 
-// cout <<sizeof(&ptr)<<endl;
+cout <<sizeof(&ptr)<<endl;
 
-// int a[20] = {3,5,6,7,6};
-//  cout << &a<<endl;
-//  cout << a<< endl;
-//  cout <<&a[0] <<endl;
+int a[20] = {3,5,6,7,6};
+ cout << &a<<endl;
+ cout << a<< endl;
+ cout <<&a[0] <<endl;
 
-//  int *p = &a[20];
+ int *p = &a[20];
 
-//  cout <<&p<<endl;
+ cout <<&p<<endl;
 
-// int arr [10] ;
-// int arr = {2,4,5,6,7,8,9};
+int arr [10] ;
+int arr = {2,4,5,6,7,8,9};
 
-// int *ptr = &arr[0];
-// cout << ptr <<endl;
-// ptr = ptr + 1;
-// cout << ptr <<endl;
+int *ptr = &arr[0];
+cout << ptr <<endl;
+ptr = ptr + 1;
+cout << ptr <<endl;
 
-// char ch[6] = "abcd";
-// int arr[5] = {1,2,3,4,5};
-// cout <<arr <<endl;
-// cout <<ch <<endl;
+char ch[6] = "abcd";
+int arr[5] = {1,2,3,4,5};
+cout <<arr <<endl;
+cout <<ch <<endl;
 
-// char *c= &ch[0];
-// cout <<&ch <<endl;
+char *c= &ch[0];
+cout <<&ch <<endl;
 
-// char ch[6] ="abcde";
-// char *p= &ch[0];
+char ch[6] ="abcde";
+char *p= &ch[0];
 
-// print(p);
+print(p);
 
 
 
