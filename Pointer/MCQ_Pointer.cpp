4ade08e34cf8f45278374491cd9f0c6bd5a06d44
istekhar44 <67890istekharalam@gmail.com>
2025-05-08 07:50:16 -0700

    vector

--- Pointer/MCQ_Pointer.cpp
+++ Pointer/MCQ_Pointer.cpp
@@ -23,13 +23,37 @@ int main(){
     */
 
 
-    int first = 8;
+/*    int first = 8;
     int second = 12;
     int * third = &second;
     int** fourth = &third ;
     first = *third;
     *third = *third +2 ;
     cout <<first <<" " <<second <<endl;
+    Ans = 12  14*/
+
+
+   /*int arr[4]= {3,4,5,6} ;
+   cout <<*(arr)<<" "<<*(arr +1)<<endl;
+   Ans= 3  4  //Because it indicates zero position of the array : *(arr)
+*/
+
+
+/*  int arr [6]= {3,5,7,8};
+  cout <<arr << "  "<<arr +1 <<endl;
+//   Ans = print the address of the first element 
+// arr   OR   &arr   OR  &arr[0]  all are same*/
+
+
+
+/*int arr [] = {11, 12, 13,14,15};
+cout<<*(arr)<<" "<<*(arr +3);
+//Ans= 11  14*/
+
+
+
+
+
 
 
 
