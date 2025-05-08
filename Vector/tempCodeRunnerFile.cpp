e2285f3e1665ba8d62661ed65cc7c3548a958f31
istekhar44 <67890istekharalam@gmail.com>
2025-05-08 08:55:55 -0700

    Leet Code

---
+++ Vector/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+for(int i : vec){
+    //     cout<<i <<endl;
+
+    // }
--- Vector/vector2.cpp
+++ Vector/vector2.cpp
@@ -3,7 +3,93 @@
 using namespace std;
 
 int main(){
-    vector<int>vec;
-    return 0;
+    //initilization of the vector
+   /* vector<int>vec;
+    return 0;*/
+
+
+
+    //initlize the vector
+   /* vector<int>hellow = {3,5,7,9};
+    cout<<"print the vector"<<endl;
+    for(int num: hellow){
+        cout<<num<<" ";
+    }
+    return 0;*/
+
+
+    // vector<int>vec(3,5);
+    // for( int i : vec){
+    //     cout<< i <<endl;
+    // }
+
+
+    // //charactor vector
+    // vector<char>hellow ={'c','d','e'};   //char write in the single colun
+    // for(char i: hellow){          //this is four each loop
+    //     cout<<i<<endl;
+
+    // }
+    // cout<<"Size of the vector"<< hellow.size()<<endl;
+    // return 0;
+
+    // //charactor vector
+    // vector<char>hellow ={'c','d','e'};   //char write in the single colun
+    // for(char i: hellow){          //this is four each loop
+    //     cout<<i<<endl;
+
+    // }
+    // cout<<"Size of the vector"<< hellow.size()<<endl;
+    // return 0;
+
+
+    //add the element in the vector
+    // vector<int>vec(5,7);
+
+    // add the element in the vector 
+    // vec.push_back(4);
+    // vec.push_front(5);
+    // for(int i: vec){
+    //     cout<<"Print the vector"<<i<endl;
+
+    // }
+    // //print the size of the vector
+    // cout<<vec.sizeof()<<endl;
+
+
+
+
+    // vector<int>vec;
+    // vec.push_back(3);
+    // vec.push_back(3);
+    // vec.push_back(3);
+    // cout<<vec.size()<<endl;
+    // cout<<vec.capacity()<<endl;
+    // for(int i : vec){
+    //     cout<<i <<endl;
+
+    // }
+
+
+
+
+    // Leet code problem   find the unique element using the exor opertor
+class solution {
+    public :
+    int singleNumber(vector<int>& num){
+        int ans = 0;
+        for(int val: nums){
+            ans^=val;
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
+
 
 }
