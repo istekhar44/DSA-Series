306b49d10de784ba666b1e165da8821f9f01d503
istekhar44 <67890istekharalam@gmail.com>
2025-05-09 07:49:35 -0700

    Referance varible

--- Pointer/REference.cpp
+++ Pointer/REference.cpp
@@ -1,4 +1,6 @@
-#include<iostream>
+// Use of the post and the pre increment
+
+/*#include<iostream>
 using namespace std;
 int main(){
     int i = 5;
@@ -9,7 +11,38 @@ int main(){
     cout<<j++<<endl;  //ans = 5
     cout<<i<<endl;    //ans = 6
     
-    cout <<"{Print MY name is khan}"
+    
+
+return 0;
+}*/
+ 
+#include<iostream>
+using namespace std;
+
+     //Create the referance varible fun  (Bad Practise)
+int & fun(int a ){
+    int num = a;
+    int &ans = num;
+    return ans;
+}
+      //Create the refance varible    (Bad Practice)
+int &update(int n){
+    int a = n;             
+    int &ans = a;
+    return ans;
+}
+int main(){
+    int a = 10;
+    int &ref=a;  //creating the reference varible
+
+    cout<<"Value Of A:"<<a <<endl;        // Output: 10
+    cout<<"ref:"<<ref<<endl;      // Output: 10
+
+    ref = 20; //Change the referance vaible
+    cout<<"Upadate the ref varible:" <<ref<<endl;  //Output = 20
+    cout<<"Update the Ref varible"<<a<<endl;  //Output = 20
+
+    fun(a);
 
 return 0;
 }
