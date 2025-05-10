b8f26d508811e9578b7f359df58608b4c2697123
istekhar.44 <67890istekharalam@gmail.com>
2025-05-10 09:19:21 -0700

    remove the dynamic memory

--- Dynamic memory/dynamicMem.cpp
+++ Dynamic memory/dynamicMem.cpp
@@ -1,26 +1,2 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int n ;
-    cin>>
-return 0;
-}
-hellow 
-#include<iostream>
-using namespace std;
-int main(){
-return 0;
-}
 
-
-#include<iostream>
-using namespace std;
-int main(){
-return 0;
-}
-#include<iostream>
-using namespace std;
-int main(){
-return 0;
-}
 
