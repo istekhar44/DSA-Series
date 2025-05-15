ef589bb23509d61d73623009bda8590d232e7940
istekhar.44 <67890istekharalam@gmail.com>
2025-05-15 07:20:33 -0700

    oop_concept

--- OOPs/inheritance.cpp
+++ OOPs/inheritance.cpp
@@ -19,7 +19,7 @@ public :
         this->weight = w;
 
     }
-
+    
 };
 
 class male : public human{
---
+++ OOPs/parameterConstructor.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+class Teacher{
+    public;
+    string name;
+    string dept;
+    int salery;
+    int id;
+    int date;
+
+    //method // memberfunction 
+    void changeDept(string newDept){
+        dept = newDept;
+    }
+
+  //access the private properties in the 
+
+
+    private;
+    string father ;
+    int age;
+    int salery;
+
+
+
+
+};
+int main(){
+    Teacher t1;
+    t1.name="sunita";
+    cout<<"print name of teacher:"<<t1.
+    Teacher t2;
+    Teacher t3;
+
+
+return 0;
+}
