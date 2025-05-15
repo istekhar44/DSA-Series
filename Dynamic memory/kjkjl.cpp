9266bc13eb795dc0cb0f27a0276c0a3ca0bb2c14
istekhar.44 <67890istekharalam@gmail.com>
2025-05-15 05:28:38 -0700

    add the opps file

---
+++ Dynamic memory/kjkjl.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+    // Data members
+    int length;
+    int breadth;
+
+public:
+    // Member function to set values
+    void setValues(int l, int b) {
+        length = l;
+        breadth = b;
+    }
+
+    // Member function to calculate area
+    int area() {
+        return length * breadth;
+    }
+
+    // Member function to display dimensions
+    void display() {
+        cout << "Length: " << length << ", Breadth: " << breadth << endl;
+    }
+};
+
+int main() {
+    Rectangle rect;
+
+    rect.setValues(10, 5);
+    rect.display();
+    cout << "Area: " << rect.area() << endl;
+
+    return 0;
+}
