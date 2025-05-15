b58192da5b0dd2872c0191ce88722faad14c9182
istekhar.44 <67890istekharalam@gmail.com>
2025-05-15 09:08:40 -0700

    Fix variable naming and constructor implementation in Teacher class; add parametrized constructor for student class

--- OOPs/parameterConstructor.cpp
+++ OOPs/parameterConstructor.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 using namespace std;
 class Teacher{
-    public;
+    
+    public:
     string name;
     string dept;
-    int salery;
+    int Salery;
     int id;
     int date;
 
@@ -12,20 +13,62 @@ class Teacher{
     void changeDept(string newDept){
         dept = newDept;
     }
+     
+   public:
+    //Set the value for all the teacher 
+    Teacher(){
+        dept="Electronic and Communication Engineer\n"<<endl;
+    }
 
-  //access the private properties in the 
+ //access the private properties in the 
+ void setSalery(double s){    // Edit the selary
+   Salery= s;
+ }
+
+ void getSalery(int s){
+    Salery = s           //access the private member 
+ }
+    
+     ///making the constructor 
+     Teacher(){
+        cout<<"Hellow i am an constructor\n"<<endl;
+     }
 
 
-    private;
+    private:
     string father ;
     int age;
     int salery;
 
 
 };
+class student{
+    int name ;
+    int age;
+    //parametrised constructor 
+    student(int  s, int n , string s,){
+        name = mohit
+    }
+
+
+
+};
+class bank{
+    private:
+    string account ;
+    string username;
+    double balance;
+    string username
+    public:
+
+}
+
 int main(){
     Teacher t1;
     t1.name="sunita";
+    t1.dept="ECE";
+    t1.salery=2000;
+
     cout<<"print name of teacher:"<<t1.name<<endl;
     Teacher t2;
     Teacher t3;
---
+++ OOPs/parametrised_constructor.cpp
@@ -0,0 +1,97 @@
+/*#include<iostream>
+using namespace std;
+class student {
+    public :
+    student(string n, int a, int r, int c){
+        name = n;
+        age = a;
+        roll = r;
+        class = c;
+    }
+
+    void display(){
+        cout<<"Name"<< name<< "Age"<<age<<"Roll"<<roll<<endl;
+    }
+
+
+};
+int main(){
+    //pass multiple argument to the constriuctor 
+    student s1("mohit", 20, 042,);
+    //print the student 1 info 
+    s1.display();
+return 0;
+}*/
+
+
+
+
+
+
+
+    // Program 2   making the copy constructor
+
+/* #include<iostream>
+using namespace std;
+class student {
+    public :
+    student(string name, int age, int roll, int class){
+        this->name = n;
+        this->age = a;
+        this->roll = r;
+        this->class = c;
+    }
+         
+      //making the copy constructor
+      student(student &or)
+
+
+    void display(){
+        cout<<"Name"<< name<< "Age"<<age<<"Roll"<<roll<<endl;
+    }
+
+
+};
+int main(){
+    //pass multiple argument to the constriuctor 
+    student s1("mohit", 20, 042,);
+    //print the student 1 info 
+    s1.display();
+return 0;  */   
+
+
+
+#include<iostream>
+using namespace std;
+class human{
+    public :
+    int height;
+    int age ;
+    int Weight;
+
+    public:
+    int getAge(){
+        return-> age
+    }
+   void setWeight(){
+        this-> weight = w;
+    }
+
+};
+class male: public human{
+    public:
+    string color;
+
+    void sleep(){
+        cout<<"Male Sleeping"<<endl;
+    }
+};
+int main(){
+    male object1;
+    object1.age= 34;
+    cout<<"print the age:"<<object1.age<<endl;
+    cout<<object1.age<<endl;
+    cout<<object1.height<<endl;
+return 0;
+}
+
