6347af3d42ab963aadeacb39a4c452b4b4785a63
istekhar44 <67890istekharalam@gmail.com>
2025-05-25 20:50:44 +0530

    Graph

--- Graph/basic.cpp
+++ Graph/basic.cpp
@@ -3,47 +3,45 @@
 #include <list>
 
 using namespace std;
-class graph{
-    public :
-    unorded_map<int , list <int> > adj;
 
+class graph {
+public:
+    unordered_map<int, list<int>> adj;  // Adjacency list to store the graph
 
-    void addEdge (int u , int v , bool direction){
-        add[u].push_back(v):
-        if (direction == 0 ) {
-            adj[v].push_back(u);
+    void addEdge(int u, int v, bool direction) {
+        adj[u].push_back(v);  // Fixed typo: changed 'add' to 'adj'
+        if (direction == 0) {
+            adj[v].push_back(u);  // If undirected, add edge in both directions
         }
-
     }
-    void printAdjList(){
-        for(auto i :adj){
-            cout<<i.first<<" ->";
-            for(auto j: i.second){
-                cout<< j << " ,";
+
+    void printAdjList() {
+        for (auto i : adj) {
+            cout << i.first << " -> ";
+            for (auto j : i.second) {
+                cout << j << ", ";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
-
 };
-int main(){
-    int n ;
-    cout<<"enter the no of Edge "<<endl;
-    cin>>n;
+
+int main() {
+    int n;
+    cout << "Enter the number of nodes: " << endl;  // Changed from "no of Edge" to be more accurate
+    cin >> n;
 
     int m;
-    cout<<"Enter the number od edge "<<endl;
-    cin>>m;
+    cout << "Enter the number of edges: " << endl;  // Fixed typo: "od" to "of"
+    cin >> m;
 
     graph g;
-    for(int i = 0 ;i<m; i++){
-        int u ,v;
-        cin>>u ,v;
-        //create an undirected graph
-        g.addEdge(u,v,0);
+    for (int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;  // Fixed: changed comma to space for input
+        // Create an undirected graph
+        g.addEdge(u, v, 0);
     }
     g.printAdjList();
     return 0;
-    
-
 }
